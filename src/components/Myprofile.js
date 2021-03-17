0d74077eb9fbd3b1b6c9590b4ab5d94614afd253
Gaurav <aadeezcity@gmail.com>
2021-03-17 18:48:37 +0530

    button will not work until values will be inserted in text

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -4,14 +4,19 @@ const Myprofile = ({location}) => {
     console.log(location);
     return (
         <div>
-             <div>
+            
+             <input type="text" placeholder="first-name" value={location.state.fName }/>
+          <input type="text" placeholder="last-name" value={location.state.lName} />
+          <input type="text" placeholder="email" value={location.state.email} />
+          <input type="text" placeholder="Contact Number" value={location.state.num}/>
+             {/* <div>
       <div> first name of user <strong>{location.state.fName }</strong></div>
       <br />
       <div>Last Name of user <strong>{location.state.lName}</strong></div>
       <div>Your Email is <strong>{location.state.email} </strong> </div>
-      <div>Your Contact Number is <strong>{location.state.num}</strong> </div>
+      <div>Your Contact Number is <strong>{location.state.num}</strong> </div> */}
     </div>
-        </div>
+        
     )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,45 +1,71 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
+const Signup = ({ history }) => {
+  const [fName, setFname] = useState();
+  const [lName, setLname] = useState();
+  const [email, setEmail] = useState();
+  const [num, setNum] = useState();
 
-const Signup = ({history}) => {
-    const[fName,setFname] = useState()
-    const[lName,setLname] = useState()
-    const[email,setEmail] = useState()
-    const[num,setNum] = useState()
-    
-    const changeFname = (e) => {
-        setFname(e.target.value);
-    }
-    const changeLname = (e) => {
-        setLname(e.target.value)
-    }
-    const changeEmail = (e) => {
-        setEmail(e.target.value)
-    }
-    const changeNum = (e) => {
-        setNum(e.target.value)
-    }
+  const changeFname = (e) => {
+    setFname(e.target.value);
+  };
+  const changeLname = (e) => {
+    setLname(e.target.value);
+  };
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const changeNum = (e) => {
+    setNum(e.target.value);
+  };
 
-    const sendData = () => {
-        history.push("./dashboard2",{
-            fName : fName,
-            lName : lName,
-            email:email,
-            num:num
-        })
-    }
+  const sendData = () => {
+    history.push("./dashboard2", {
+      fName: fName,
+      lName: lName,
+      email: email,
+      num: num,
+    });
+  };
 
   return (
     <div>
-      
-          <input type="text" placeholder="first-name" value={fName} onChange={changeFname}/>
-          <input type="text" placeholder="last-name" value={lName} onChange={changeLname}/>
-          <input type="text" placeholder="email" value={email} onChange={changeEmail}/>
-          <input type="text" placeholder="Contact Number" value={num} onChange={changeNum}/>
-             <button onClick={sendData}>Register</button>
-
-
-     
+      <form>
+        <input
+          type="text"
+          placeholder="first-name"
+          value={fName}
+          onChange={changeFname}
+          className="check"
+        />
+        <input
+          type="text"
+          placeholder="last-name"
+          value={lName}
+          onChange={changeLname}
+          className="check"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={changeEmail}
+          className="check"
+        />
+        <input
+          type="text"
+          placeholder="Contact Number"
+          value={num}
+          onChange={changeNum}
+          className="check"
+        />
+        <button
+          disabled={!fName || !lName || !email || !num}
+          onClick={sendData}
+        >
+          Register
+        </button>
+      </form>
     </div>
   );
 };
